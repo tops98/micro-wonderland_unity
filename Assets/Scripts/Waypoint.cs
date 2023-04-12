e523c6a3484c74539496f440ea2e002246d2ccbe
Tobias Haugg <tobias.haugg@gmail.com>
2023-04-12 12:42:39 +0200

    refactor visuals of the waypoint system

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -5,13 +5,12 @@ using UnityEditor;
 
 public class Waypoint : MonoBehaviour
 {
-    // public List<Waypoint> neighbors;
     public List<Waypoint> neighbors;
-    // public GameObject accessableNeigbor;
     public Waypoint accessableNeigbor;
+    public bool isSwitch;
 
-    [HideInInspector]
-    public Mesh arrow;
+    [HideInInspector]public Waypoint predecessor;
+    [HideInInspector]public Mesh arrow;
 
     Waypoint(){
         neighbors = new List<Waypoint>();
@@ -19,19 +18,27 @@ public class Waypoint : MonoBehaviour
     
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawSphere(transform.position,0.5f);
+        isSwitch = neighbors.Count>1;
+
+        var sphereColor = Color.blue;
+        var sphereSize = 0.5f;
+        if(isSwitch){
+            sphereColor = Color.green;
+            sphereSize = 0.75f;
+        }
+        Gizmos.color = sphereColor;
+        Gizmos.DrawSphere(transform.position,sphereSize);
         DrawConnections();
     }
-    bool Same(Transform a,Transform b){
-        return a==b;
-    }
+
+    
      // very inefficent
     private void getArrow()
     {
         Debug.Log("getting arrow");
         foreach(var mesh in Resources.FindObjectsOfTypeAll<Mesh>()){
-            if(mesh.name =="arrow"){
+            if(mesh.name =="arrow")
+            {
                 arrow = mesh;
                 Debug.Log("get arrow");
                 break;
@@ -39,24 +46,40 @@ public class Waypoint : MonoBehaviour
         }
     }
     
-    private void DrawConnections(){
-        if(neighbors.Count <= 0){
+    private void DrawConnections()
+    {
+        if(neighbors.Count <= 0)
+        {
             return;
         }
 
-        foreach(Waypoint currentNeighor in neighbors){
-            if(currentNeighor == null){
+        Vector3 center = new Vector3();
+        foreach(Waypoint currentNeighor in neighbors)
+        {
+            if(currentNeighor == null)
+            {
                 neighbors.Remove(currentNeighor);
                 break;
             } 
-            var center = currentNeighor.transform.position - (currentNeighor.transform.position-transform.position)/2.0f;
+            center = transform.position + Vector3.Normalize(currentNeighor.transform.position-transform.position)*2.0f;
             Gizmos.color = Color.yellow;
+            if(isSwitch)
+            {
+                if(currentNeighor == accessableNeigbor)
+                {
+                    Gizmos.color = Color.green;
+                    Gizmos.DrawMesh(arrow,center,Quaternion.LookRotation(currentNeighor.transform.position-transform.position,Vector3.forward));
+                }else
+                {
+                    Gizmos.color = Color.red;
+                }
+            }  
+
             Gizmos.DrawLine(transform.position,currentNeighor.transform.position);
-            Gizmos.color = Color.cyan;
-            if(arrow == null){
+            if(arrow == null)
+            {
                 getArrow();
             }
-            Gizmos.DrawMesh(arrow,center,Quaternion.LookRotation(currentNeighor.transform.position-transform.position,Vector3.forward));
         }
     }
 }
@@ -80,7 +103,8 @@ public class WaypointInspector: Editor{
             newWaypoint.AddComponent<Waypoint>();
             var wpScript = newWaypoint.GetComponent<Waypoint>();
             wpScript.arrow = waypoint.arrow;
-            
+            wpScript.predecessor = waypoint;
+
             if(wpScript.neighbors.Count == 0)
                 wpScript.accessableNeigbor =wpScript;
             
